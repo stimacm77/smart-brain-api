1c51dc4e8f4b213c970ceb860b853d3db68d5479
Mark <Markstimac77@gmail.com>
2020-12-24 19:35:38 -0800

    post change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-aerodynamic-59463',
     user : 'stimacm',
     password : '',
     database : 'smart-brain'
